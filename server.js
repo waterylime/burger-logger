6d6acd13bbdfce998fdf0b7cc17524b031a76386
waterylime46@aol.com <waterylime46@aol.com>
2020-01-15 19:54:08 -0700

    ...

--- server.js
+++ server.js
@@ -1,32 +1,24 @@
-
-const express = require("express");
+const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 const db = require('./mobles');
 
-const PORT = process.eventNames.PORT || 3000;
-const app = express ();
- 
-   
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const exphbs = require("express-handlebars");
+const exphbs = require('express-handlebars');
 
-app.engine('handlebars', exphbs({defaultLayout:'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 
 app.set('view engine', 'handlebars');
 
 let routes = require('./controllers/burgers_controller.js');
 app.use(routes);
 
-
-db.sequlize.sync().then(fuction(){
-    app.listen(PORT, ()=>{
-        console.log('Listeningt on PORT' + PORT);
-        
-    })
+db.sequlize.sync().then(function() {
+	app.listen(PORT, () => {
+		console.log('Listeningt on PORT' + PORT);
+	});
 });
-
